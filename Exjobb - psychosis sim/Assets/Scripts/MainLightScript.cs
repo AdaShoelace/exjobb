64d394a48d5ddec3a3fa8f82cbdf394ef9916b7d
DESKTOP-0RHJFR5\Master <pierre.leidbring@gmail.com>
2018-04-05 16:29:44 +0200

    changes

--- Exjobb - psychosis sim/Assets/Scripts/MainLightScript.cs
+++ Exjobb - psychosis sim/Assets/Scripts/MainLightScript.cs
@@ -9,7 +9,7 @@ namespace Pierre.Unidux {
 	public class MainLightScript : MonoBehaviour {
 
 		private Color baseColor = new Color(1,1,1,1);
-		private const float intensityScaleFactor = 0.2f;
+		private const float intensityScaleFactor = 0.1f;
 		private const float colorScaleFactor = 0.1f;
 		private float cycleTime;
 		private GameObject[] lightGroup;
@@ -26,11 +26,14 @@ namespace Pierre.Unidux {
 				.Subscribe(state => {
 					foreach(GameObject go in lightGroup) {
 						var light = go.GetComponent("Light") as Light;
-						if(light.intensity > .15f) 
-							light.intensity = state.lightIntensity;
+						if(light.range < 100) {
+							light.range += 2.0f;
+						}
+						
+						if(light.color.r > 0.2f && light.color.g > 0.2f) {
+							light.color = new Color(light.color.r - .2f, light.color.g - .2f, light.color.b, 1);
+						}
 
-						light.range -= .2f;
-						light.color = new Color(light.color.r - .2f, light.color.g - .2f, light.color.b + .2f, 1);
 						RenderSettings.ambientLight = Color.black;
 					}
 				})
@@ -40,8 +43,8 @@ namespace Pierre.Unidux {
 		
 		// Update is called once per frame
 		void Update () {
-			if(Time.time - cycleTime > 20) {
-				intensity -= Time.deltaTime * intensityScaleFactor;
+			if(Time.time - cycleTime > 2) {
+				intensity -= intensityScaleFactor;
 				Unidux.Dispatch(Actions.ActionCreator.LightIntensityAction(intensity));
 				cycleTime = Time.time;
 			}
---
+++ Exjobb - psychosis sim/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+namespace Pierre.Unidux {
+	public class SceneManager : MonoBehaviour {
+
+		// Use this for initialization
+		void Awake () {
+			var hs = gameObject.GetComponent<VRTK_HeadsetFade>();
+			hs.Fade(Color.black, .1f);
+			hs.Unfade(3.0f);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
