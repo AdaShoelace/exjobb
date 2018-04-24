83e15a1b19269c7aaad34acec66532de9830ead5
DESKTOP-0RHJFR5\Master <pierre.leidbring@gmail.com>
2018-04-24 09:25:28 +0200

    asdf

--- Exjobb - psychosis sim/Assets/Scripts/HallucinationAudio.cs
+++ Exjobb - psychosis sim/Assets/Scripts/HallucinationAudio.cs
@@ -16,28 +16,29 @@ namespace Pierre.Unidux
             foreach (AudioSource go in temp.GetComponentsInChildren<AudioSource>())
             {
                 playerSources.Add(go.transform.parent.name.ToString(), go);
+                print("Parent name:" + go.transform.parent.name);
             }
             Unidux.Subject
                         .TakeUntilDisable(this)
                         .StartWith(Unidux.State)
                         .Subscribe(state =>
                         {
-							//test
-							if(state.playPillHallucinationAudio)
-							{
-								PlayCloseProximityAudio(Resources.Load("Voices/Crowd Whispering") as AudioClip);
-							}
+                            //test
+                            if (state.playPillHallucinationAudio)
+                            {
+                                PlayCloseProximityAudio(Resources.Load("Voices/Crowd Whispering") as AudioClip);
+                            }
                         })
                         .AddTo(this);
         }
         private void PlayCloseProximityAudio(AudioClip audioClip)
         {
-			AudioSource left = playerSources["AudioLeft"];
-			left.clip = audioClip;
-			AudioSource right = playerSources["AudioRight"];
-			right.clip = audioClip;
-			left.Play();
-			right.Play();
+            AudioSource left = playerSources["AudioLeft"];
+            left.clip = audioClip;
+            AudioSource right = playerSources["AudioRight"];
+            right.clip = audioClip;
+            left.Play();
+            right.Play();
         }
     }
 }
